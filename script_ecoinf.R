e2fdf943389130f38474a06fb266b8aa7440ed05
cristimarr <cristimartinromera02@gmail.com>
2025-02-19 12:25:05 +0100

    cambio color

--- script_ecoinf.R
+++ script_ecoinf.R
@@ -4,7 +4,7 @@ df <- read.csv(file = "../ecoinf_cristinamartin/peso_altura_bellota.csv")
 
 ggplot(df, aes(x = peso_bellota, y = altura_plantula)) +
   geom_point() +
-  geom_smooth(method = "lm", se = TRUE, color = "aquamarine1") +
+  geom_smooth(method = "lm", se = TRUE, color = "#FF7F00") +
   labs(x = "Peso de la Bellota",
        y = "Altura de la Plántula")
 
